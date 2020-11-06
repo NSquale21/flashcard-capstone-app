12d03f12992c864e545003f64995f6450a2ec142
Nicole Pasquale <NSquale212@gmail.com>
2020-11-06 15:34:46 -0600

    Add routing & paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Home from './views/Home';
+import Compose from './views/Compose';
+import DecksPage from './views/Decks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/compose">
+            <Compose />
+          </Route>
+          <Route exact path="/decks/:id">
+            <DecksPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Container>
   );
 }
 
